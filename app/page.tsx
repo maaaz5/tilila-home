1ca54f0222b25e4f1ab87f7cd95c4da29ea64146
maaaz5 <mohamade.amine12345@gmail.com>
2024-08-11 23:01:18 +0100

    adding pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
 }
-
 export default HomePage;
