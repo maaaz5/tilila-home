aff5bc6d889ba2ba052092b1fa6c33bc9c4e2785
maaaz5 <mohamade.amine12345@gmail.com>
2024-08-20 22:37:18 +0100

    FormInput Component

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,5 +1,3 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const createProfileAction = async (formData: FormData) => {
@@ -14,10 +12,6 @@ function CreateProfilePage() {
       <h1 className="text-xl font-semibold mb-8 capitalize">new user</h1>
       <div className="border p-8 rounded-md max-w-lg">
         <form action={createProfileAction}>
-          <div className="mb-2">
-            <Label htmlFor="firstName">First Name</Label>
-            <Input id="firstName" name="firstName" type="text" />
-          </div>
           <Button type="submit" size={"lg"}>
             Create Profile
           </Button>
