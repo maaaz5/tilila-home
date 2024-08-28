87c58d8f3ad0ce78738bbda36bee23da54b8c0b3
maaaz5 <mohamade.amine12345@gmail.com>
2024-08-29 00:02:04 +0100

    Create property done

--- utils/actions.ts
+++ utils/actions.ts
@@ -145,12 +145,22 @@ export const createPropertyAction = async (
   const user = await getAuthUser();
   try {
     const rawData = Object.fromEntries(formData);
-    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+    const file = formData.get("image") as File;
 
-    return { message: "property created" };
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+    const validatedFile = validateWithZodSchema(imageSchema, { image: file });
+    const fullPath = await uploadImage(validatedFile.image);
+
+    await db.property.create({
+      data: {
+        ...validatedFields,
+        image: fullPath,
+        profileId: user.id,
+      },
+    });
   } catch (error) {
     return renderError(error);
   }
 
-  // redirect("/");
+  redirect("/");
 };
