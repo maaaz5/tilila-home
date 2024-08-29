6e98e7fc1c41af96def3f51f9a2fc8801c89d8e8
maaaz5 <mohamade.amine12345@gmail.com>
2024-08-29 23:21:42 +0100

    loading state for property cards

--- components/card/LoadingCards.tsx
+++ components/card/LoadingCards.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Skeleton } from "../ui/skeleton";
 
 function LoadingCards() {
-  return <div>LoadingCards</div>;
+  return (
+    <div className="mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <SkeletionCard />
+      <SkeletionCard />
+      <SkeletionCard />
+      <SkeletionCard />
+    </div>
+  );
 }
-
+export function SkeletionCard() {
+  return (
+    <div>
+      <Skeleton className="h-[300px] rounded-md" />
+      <Skeleton className="h-4 mt-2 w-3/4" />
+      <Skeleton className="h-4 mt-2 w-1/4" />
+    </div>
+  );
+}
 export default LoadingCards;
