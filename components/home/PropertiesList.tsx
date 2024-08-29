6adaeb69dae58f60290af3bbc5428e826c1c8517
maaaz5 <mohamade.amine12345@gmail.com>
2024-08-29 22:54:47 +0100

    property card setup

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,5 +1,14 @@
-function PropertiesList() {
-  return <div>PropertiesList</div>;
+import type { PropertyCardProps } from "@/utils/types";
+import PropertyCard from "../card/PropertyCard";
+
+function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {properties.map((pr) => (
+        <PropertyCard key={pr.id} property={pr} />
+      ))}
+    </section>
+  );
 }
 
 export default PropertiesList;
