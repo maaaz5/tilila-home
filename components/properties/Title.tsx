2522e590b5be455498ea849aa95151a5019919dd
maaaz5 <mohamade.amine12345@gmail.com>
2024-08-31 03:30:24 +0100

    amenities done

--- components/properties/Title.tsx
+++ components/properties/Title.tsx
@@ -1,5 +1,5 @@
 function Title({ text }: { text: string }) {
-  return <h3 className="text-lg fron-bold mb-2">{text}</h3>;
+  return <h3 className="text-lg font-bold mb-2">{text}</h3>;
 }
 
 export default Title;
