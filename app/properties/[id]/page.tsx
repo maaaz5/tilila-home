8e29a7529ae9506e405c43782f9f785ad6564247
maaaz5 <mohamade.amine12345@gmail.com>
2024-09-02 08:54:44 +0100

    setting zustand

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -24,6 +24,14 @@ const DynamicMap = dynamic(
   }
 );
 
+const DynamicBookingWrapper = dynamic(
+  () => import("@/components/booking/BookingWrapper"),
+  {
+    ssr: false,
+    loading: () => <Skeleton className="h-[200px] w-full" />,
+  }
+);
+
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
   if (!property) redirect("/");
@@ -64,6 +72,11 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           {/* calendar */}
+          <DynamicBookingWrapper
+            propertyId={property.id}
+            price={property.price}
+            bookings={property.bookings}
+          />
         </div>
       </section>
       {ReviewDoesNotExist && <SubmitReview propertyId={property.id} />}
--- components/booking/BookingWrapper.tsx
+++ components/booking/BookingWrapper.tsx
@@ -1,5 +1,31 @@
-function BookingWrapper() {
-  return <div>BookingWrapper</div>;
+"use client";
+
+import { useProperty } from "@/utils/store";
+import { Booking } from "@/utils/types";
+import BookingCalendar from "./BookingCalendar";
+import BookingContainer from "./BookingContainer";
+import { useEffect } from "react";
+
+type BookingWrapperProps = {
+  propertyId: string;
+  price: number;
+  bookings: Booking[];
+};
+
+function BookingWrapper({ propertyId, price, bookings }: BookingWrapperProps) {
+  useEffect(() => {
+    useProperty.setState({
+      bookings,
+      propertyId,
+      price,
+    });
+  }, []);
+  return (
+    <>
+      <BookingCalendar />
+      <BookingContainer />
+    </>
+  );
 }
 
 export default BookingWrapper;
