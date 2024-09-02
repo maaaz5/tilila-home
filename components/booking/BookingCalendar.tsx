7b760810cd884ea23a496071ade90c6b0c192427
maaaz5 <mohamade.amine12345@gmail.com>
2024-09-02 11:49:20 +0100

    Blocked Periods & Disabled Dates

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -15,14 +15,38 @@ import {
 function BookingCalendar() {
   const currentDate = new Date();
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+  const bookings = useProperty((state) => state.bookings);
+  const { toast } = useToast();
+
+  const blockedPeriods = generateBlockedPeriods({
+    bookings,
+    today: currentDate,
+  });
+
+  const unavailableDates = generateDisabledDates(blockedPeriods);
+  console.log(unavailableDates);
 
   useEffect(() => {
+    const selectedRange = generateDateRange(range);
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: "Some dates are booked. Please select again",
+        });
+        return true;
+      }
+      return false;
+    });
+
     useProperty.setState({
       range,
     });
   }, [range]);
+
   return (
     <Calendar
+      disabled={blockedPeriods}
       mode="range"
       defaultMonth={currentDate}
       selected={range}
